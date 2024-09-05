c87a75cbc29ad9e7e8a2db11959220b2d85cd5de
Michael Katsoulis <michaelkatsoulis@gmail.com>
2024-09-05 17:33:33 +0200

    Update submodules. Improve Dashboard

--- src/visualisation.py
+++ src/visualisation.py
@@ -4,8 +4,8 @@ import pandas as pd
 import seaborn as sns
 
 
+sns.set_theme(style="whitegrid")
 
-sns.set_theme(style='whitegrid')
 
 def plot_lines(sampled_df: pd.DataFrame, interesting_cols: List[str], fig_path=None):
     # Plotting the interesting columns with improved performance and cleaner rendering for high density of points
@@ -15,65 +15,82 @@ def plot_lines(sampled_df: pd.DataFrame, interesting_cols: List[str], fig_path=N
         ax.plot(subset.index, subset[col], label=col, linewidth=0.5)
         # ax.scatter(sampled_df.index, sampled_df[col], label=col)
 
-    ax.set_xlabel('Timestamps')
-    ax.set_ylabel(', '.join(interesting_cols))  # Set y-label based on input columns
-    ax.set_title('Interesting Columns Over Time')
+    ax.set_xlabel("Timestamps")
+    ax.set_ylabel(", ".join(interesting_cols))  # Set y-label based on input columns
+    ax.set_title("Interesting Columns Over Time")
     ax.legend(title="Legend")  # Add a legend with a title
     ax.grid(True)
-    
+
     plt.tight_layout(pad=3.0)  # Optimize space in the layout
 
     if fig_path is not None:
-        fig.savefig(fig_path, format='png', dpi=300)  # Save as high-resolution image if path is provided
+        fig.savefig(
+            fig_path, format="png", dpi=300
+        )  # Save as high-resolution image if path is provided
     plt.show()
     return fig
 
 
-def multi_plot_line(sampled_df: pd.DataFrame, columns_sets_per_plot: List[List[str]], fig_path=None):
-    fig, axes = plt.subplots(nrows=len(columns_sets_per_plot), ncols=1, figsize=(10, 6 * len(columns_sets_per_plot)))
+def multi_plot_line(
+    sampled_df: pd.DataFrame, columns_sets_per_plot: List[List[str]], fig_path=None
+):
+    fig, axes = plt.subplots(
+        nrows=len(columns_sets_per_plot),
+        ncols=1,
+        figsize=(10, 6 * len(columns_sets_per_plot)),
+    )
     if len(columns_sets_per_plot) == 1:
-        axes = [axes]  # Ensure axes is always a list for consistency in single subplot scenarios
+        axes = [
+            axes
+        ]  # Ensure axes is always a list for consistency in single subplot scenarios
 
     for ax, cols in zip(axes, columns_sets_per_plot):
         for col in cols:
             ax.plot(sampled_df.index, sampled_df[col], label=col)
-        ax.set_xlabel('Timestamps')
-        ax.set_ylabel(', '.join(cols))
-        ax.set_title('Plot of ' + ', '.join(cols))
+        ax.set_xlabel("Timestamps")
+        ax.set_ylabel(", ".join(cols))
+        ax.set_title("Plot of " + ", ".join(cols))
         ax.legend()
         ax.grid(True)
 
     plt.tight_layout()
 
     if fig_path is not None:
-        fig.savefig(fig_path, format='png', dpi=300)  # Save as high-resolution image if path is provided
+        fig.savefig(
+            fig_path, format="png", dpi=300
+        )  # Save as high-resolution image if path is provided
     plt.show()
 
 
 import contextlib
 from rich.errors import NotRenderableError
-def rich_display_dataframe(df: pd.DataFrame, title="Dataframe", lim_cols = 10, lim_rows = 20) -> None:
+
+
+def rich_display_dataframe(
+    df: pd.DataFrame, title="Dataframe", lim_cols=10, lim_rows=20
+) -> None:
     """Display dataframe as table using rich library.
-        Args:
-            df (pd.DataFrame): dataframe to display
-            title (str, optional): title of the table. Defaults to "Dataframe".
-        Raises:
-            NotRenderableError: if dataframe cannot be rendered
-        Returns:
-            rich.table.Table: rich table
-        """
+    Args:
+        df (pd.DataFrame): dataframe to display
+        title (str, optional): title of the table. Defaults to "Dataframe".
+    Raises:
+        NotRenderableError: if dataframe cannot be rendered
+    Returns:
+        rich.table.Table: rich table
+    """
     from rich import print
     from rich.table import Table
+
     # ensure dataframe contains only string values
     df = df.astype(str)
     table = Table(title=title)
     for c, col in enumerate(df.columns):
-        if c>lim_cols: 
+        if c > lim_cols:
             print(f"Skipping the rest of the columns after {lim_cols}")
             break
         table.add_column(col)
     for r, row in enumerate(df.values):
-        if r>lim_rows:
+        if r > lim_rows:
             print(f"Skipping the rest of the rows after {lim_rows}")
             break
         with contextlib.suppress(NotRenderableError):
@@ -82,3 +99,71 @@ def rich_display_dataframe(df: pd.DataFrame, title="Dataframe", lim_cols = 10, l
     print(table)
 
 
+def hvplot_df_by_col(df: pd.DataFrame, cols: List[str], xlabel="", ylabel=""):
+    """
+    Recursively plot data from a DataFrame using Holoviews for each specified column.
+
+    This function takes a DataFrame and a list of column names, plotting each column
+    using Holoviews. If multiple columns are specified, it overlays the plots of all
+    columns. The function handles missing data by dropping NA values before plotting.
+
+    Args:
+        df (pd.DataFrame): The DataFrame containing the data to plot.
+        cols (List[str]): A list of column names to be plotted. The function plots
+                          the first column and then recursively calls itself to plot
+                          the remaining columns, overlaying each subsequent plot.
+
+    Returns:
+        hvPlot object if columns are provided, otherwise None.
+
+    Example:
+        >>> hvplot_df_by_col(df, ['column1', 'column2'])
+        This will overlay the plots of 'column1' and 'column2' after dropping NAs.
+    """
+    if len(cols) < 1:
+        return None
+    name = cols.pop(0)
+    while name not in df.columns and len(cols) > 1:
+        print(f"[yellow bold]🚧WARNING: Could not find {name} in data... skipping.")
+        name = cols.pop(0)
+
+    if len(cols) > 0:
+        return df[name].dropna().hvplot(
+            alpha=0.7, grid=True, xlabel=xlabel, ylabel=ylabel
+        ) * hvplot_df_by_col(df, cols, xlabel=xlabel, ylabel=ylabel)
+    else:
+        return (
+            df[name].dropna().hvplot(alpha=0.7, grid=True, xlabel=xlabel, ylabel=ylabel)
+        )
+
+
+def power_plot(df: pd.DataFrame, voltage_col="Voltage", current_col="Current"):
+    power = df[voltage_col] * df[current_col]
+
+    # filter out nan where there were missing messages
+    power = power[pd.notna(power)] / 1000
+
+    # render plot:
+    return power.hvplot.area(
+        ylabel="Power (kW)",
+        color="green",
+        alpha=0.7,
+        grid=True,
+        line_color="darkgreen",
+        line_alpha=0.6,
+    )
+
+
+def plot_temperatures(
+    df: pd.DataFrame,
+):
+    temp_cols = [
+        "TempCurrCool1",
+        "TempCurr1",
+        "ElectricMachineTemperature1",
+        "InverterTemperature1",
+        "TempCurrRotor1",
+    ]
+
+    print("Plotting temperatures: ", temp_cols)
+    return hvplot_df_by_col(df=df, cols=temp_cols, ylabel="Temperature ( ºC )")
--- src/webapp.py
+++ src/webapp.py
@@ -1,24 +1,30 @@
+from typing import List
 import panel as pn
 import hvplot.pandas
 import pandas as pd
 import numpy as np
+import visualisation as vis
 
-pn.extension("echarts")
+pn.extension()
+# pn.extension(theme="dark")
 
+# STYLING
+BRAND_COLOR = "teal"
+BRAND_TEXT_ON_COLOR = "white"
 
-def remove_empty_vals(df, selected_columns):
-    """Filters each of the selected columns, returning a List of Series, each without any empty values."""
-
-    pass
+CARD_STYLE = {
+    "box-shadow": "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px",
+    "padding": "10px",
+    "border-radius": "5px",
+}
 
 
 # # Assuming df_mdf_filtered is the DataFrame you want to display and interact with
-def create_app(df):
+def create_app(df: pd.DataFrame):
     cols = list(df_mdf_filtered.columns)
 
     # Create a checkbox group for selecting columns to plot
     column_selector = pn.widgets.CheckBoxGroup(
-        name="Select Columns",
         value=["Voltage", "Current"],  # Default selected values
         options=cols,
         # inline=True,
@@ -26,50 +32,28 @@ def create_app(df):
 
     # Function to update the plot based on selected columns
     @pn.depends(column_selector.param.value)
-    def update_plot(selected_columns):
+    def update_plot(selected_columns: List[str]):
         if not selected_columns:
             return "Please select at least one column to plot."
-        vals = df_mdf_filtered[selected_columns]
-
-        # TODO this is wrong. We need to add plots for each column on its own.
-        # TODO rewrite and seperate.
-        vals = vals[np.all(pd.notna(vals), axis=1)]
-        print(vals)
-        return vals.hvplot()
-
-    # TODO trash gauge. It's just filler.
-    gauge = {
-        "tooltip": {"formatter": "{a} <br/>{b} : {c}%"},
-        "series": [
-            {
-                "name": "Gauge",
-                "type": "gauge",
-                "detail": {"formatter": "{value}%"},
-                "data": [{"value": 50, "name": "Value"}],
-            }
-        ],
-    }
-    gauge_pane = pn.pane.ECharts(gauge, width=400, height=400)
-
-    slider = pn.widgets.IntSlider(value=50, start=0, end=100)
-
-    slider.jscallback(
-        args={"gauge": gauge_pane},
-        value="""
-    gauge.data.series[0].data[0].value = cb_obj.value
-    gauge.properties.data.change.emit()
-    """,
-    )
+        return vis.hvplot_df_by_col(df=df, cols=selected_columns)
 
     # Layout the components
     app_layout = pn.Row(
-        pn.Column(slider, gauge_pane),
         pn.Column(
-            "## Data Visualisation App",
-            update_plot,
+            "## Temperatures",
+            vis.plot_temperatures(df=df),
             "---",
-            "Select columns to plot:",
-            column_selector,
+            "## Data Visualiser",
+            pn.panel(update_plot),
+            "---",
+            "### Select columns to plot:",
+            pn.panel(column_selector, max_height=100),
+        ),
+        pn.Column(
+            "## Power Consumption",
+            pn.panel(
+                vis.power_plot(df=df, voltage_col="Voltage", current_col="Current")
+            ),
         ),
     )
 
@@ -80,7 +64,7 @@ print("Re-loading the mf4 file")
 
 # TODO add a file selector GUI.
 df_mdf_filtered = pd.read_csv(
-    "../processed_files/filtered_61494E67_00000039_00000001-0B1D281D351973497D3A29B6168B8C9C2A6143BC0B521D2EE92BB91BB844ECC4.csv",
+    "processed_files/filtered_61494E67_00000039_00000001-0B1D281D351973497D3A29B6168B8C9C2A6143BC0B521D2EE92BB91BB844ECC4.csv",
 )
 
 df_mdf_filtered.set_index("timestamps", inplace=True)
@@ -93,11 +77,63 @@ minutes, seconds = divmod(remainder, 60)
 time_string = f"{int(hours)}h {int(minutes)}m {int(seconds)}s"
 
 
+print(f"DEBUG: total_power = ")
+total_power = df_mdf_filtered["Voltage"] * df_mdf_filtered["Current"]
+total_power = total_power[pd.notna(total_power)].sum()
+total_power_kwh = total_power / 1000 / 60 / 60
+
+
+indicators = pn.FlexBox(
+    pn.indicators.String(
+        value=time_string,
+        name="Total Runtime",
+        # format="{value:,s}",
+        styles=CARD_STYLE,
+    ),
+    pn.indicators.Number(
+        value=total_power_kwh,
+        name="Power Consumed (kWh)",
+        format="{value:,.3f}",
+        styles=CARD_STYLE,
+    ),
+    pn.indicators.Number(
+        value=4.20,
+        name="Distance Travelled (km)",
+        format="{value:,.2f}",
+        styles=CARD_STYLE,
+    ),
+    pn.indicators.Number(
+        value=6.9,
+        name="CO2 Emmisions Saved (kg)",
+        format="{value:,.2f}",
+        styles=CARD_STYLE,
+    ),
+    pn.indicators.Number(
+        value=9.999,
+        name="Electricity Cost (chf)",
+        format="{value:,.2f}",
+        styles=CARD_STYLE,
+    ),
+    margin=(20, 5),
+)
+
+
+sidebar = [
+    "# IREX Dashboard\n---",
+    pn.Spacer(sizing_mode="stretch_both"),
+    pn.panel(
+        "https://static.wixstatic.com/media/7429ff_c332d4103e494ce0b1149da82afde237~mv2.png/v1/fill/w_248,h_72,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Copy%20of%20NewLogo_rectangle_4QT.png"
+    ),
+]
+
+
+######################
+#   Rendering
+######################
 template = pn.template.FastListTemplate(
     title="4QT IREX Log Parser",
-    sidebar=[
-        f"## Runtime for Log: {time_string}\n---",
-    ],
+    sidebar=sidebar,
+    accent="#4099da",
 )
 
 # TODO create pannels to plot each of: Motion, Temperature, Battery, Emissions.
@@ -105,6 +141,7 @@ app = create_app(df_mdf_filtered)
 
 # TODO in sideline add summaries for: runtime, power used, power generaed, distance travelled
 
+template.main.append(indicators)
 template.main.append(app)
 template.servable()
 
