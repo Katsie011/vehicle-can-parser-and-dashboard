bf359205ec84ff3fda806bf9a4bc0479e618c3e3
Michael Katsoulis <michaelkatsoulis@gmail.com>
2024-09-06 11:18:48 +0200

    Interactive file selection

--- src/webapp.py
+++ src/webapp.py
@@ -7,80 +7,84 @@ import numpy as np
 import visualisation as vis
 import utils
 
-
 pn.extension()
 # pn.extension(theme="dark")
 
 
+# File selector GUI added for user to select or upload a file.
+# file_input = pn.widgets.FileInput(accept=".csv", name="Upload CSV File")
+file_input = pn.widgets.FileSelector(
+    name="Upload CSV File", directory=".", file_pattern="*.csv"
+)
+
+
 # # Assuming df_mdf_filtered is the DataFrame you want to display and interact with
-def create_app(df: pd.DataFrame):
-    cols = list(df_mdf_filtered.columns)
+# @pn.depends(file_input.param.filename, watch=True)
+def create_app(file_list=None):
+    print("Recieved file:", file_list)
+    if file_list is not None and file_list != []:
+        # df = pd.read_csv(io.BytesIO(file))
+        file = file_list[0]
+        df = pd.read_csv(file)
+        df.set_index("timestamps", inplace=True)
 
-    # Create a checkbox group for selecting columns to plot
-    column_selector = pn.widgets.CheckBoxGroup(
-        value=["Voltage", "Current"],  # Default selected values
-        options=cols,
-        # inline=True,
-    )
+        # df = pd.read_csv(io.BytesIO(df_mdf_filtered))
+        # df.set_index("timestamps", inplace=True)
+        indicators = utils.get_indicators(df=df)
 
-    # Function to update the plot based on selected columns
-    @pn.depends(column_selector.param.value)
-    def update_plot(selected_columns: List[str]):
-        if not selected_columns:
-            return "Please select at least one column to plot."
-        return vis.hvplot_df_by_col(df=df, cols=selected_columns)
+        cols = list(df)
 
-    # Layout the components
-    app_layout = pn.Row(
-        pn.Column(
-            "## Temperatures",
-            vis.plot_temperatures(df=df),
-            "---",
-            "## Data Visualiser",
-            pn.panel(update_plot),
-            "---",
-            "### Select columns to plot:",
-            pn.panel(column_selector, max_height=100),
-        ),
-        pn.Column(
-            "## Power Consumption",
-            pn.panel(
-                vis.power_plot(df=df, voltage_col="Voltage", current_col="Current")
+        # Create a checkbox group for selecting columns to plot
+        column_selector = pn.widgets.CheckBoxGroup(
+            value=["Voltage", "Current"],  # Default selected values
+            options=cols,
+            # inline=True,
+        )
+
+        # Function to update the plot based on selected columns
+        @pn.depends(column_selector.param.value)
+        def update_plot(selected_columns: List[str]):
+            if not selected_columns:
+                return "Please select at least one column to plot."
+            return vis.hvplot_df_by_col(df=df, cols=selected_columns)
+
+        # Layout the components
+        app_layout = pn.Column(
+            pn.Row(indicators),
+            pn.Row(
+                pn.Column(
+                    "## Temperatures",
+                    vis.plot_temperatures(df=df),
+                    "---",
+                    "## Data Visualiser",
+                    pn.panel(update_plot),
+                    "---",
+                    "### Select columns to plot:",
+                    pn.panel(column_selector, max_height=100),
+                ),
+                pn.Column(
+                    "## Power Consumption",
+                    pn.panel(
+                        vis.power_plot(
+                            df=df, voltage_col="Voltage", current_col="Current"
+                        )
+                    ),
+                ),
             ),
-        ),
-    )
+        )
+        return app_layout
 
-    return app_layout
+    else:
+        return file_input  # Return an empty DataFrame if no file is selected
 
 
 print("Re-loading the mf4 file")
 
 
-# File selector GUI added for user to select or upload a file.
-file_input = pn.widgets.FileInput(accept=".csv", name="Upload CSV File")
-
-
-@pn.depends(file_input.param.value, watch=True)
-def load_data():
-    if file_input.value is not None:
-
-        df = pd.read_csv(io.BytesIO(file_input.value))
-        df.set_index("timestamps", inplace=True)
-        return df
-    else:
-        return pd.DataFrame()  # Return an empty DataFrame if no file is selected
-
-
-df_mdf_filtered = load_data()
-
-
-indicators = utils.get_indicators(df=df_mdf_filtered)
-
-
 sidebar = [
     "# IREX Dashboard\n---",
     pn.Spacer(sizing_mode="stretch_both"),
-    file_input,
+    # file_input,
     pn.Spacer(sizing_mode="stretch_both"),
     pn.panel(
         "https://static.wixstatic.com/media/7429ff_c332d4103e494ce0b1149da82afde237~mv2.png/v1/fill/w_248,h_72,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Copy%20of%20NewLogo_rectangle_4QT.png"
@@ -91,19 +95,19 @@ sidebar = [
 ######################
 #   Rendering
 ######################
+
+interactive_app = pn.bind(create_app, file_input)
 template = pn.template.FastListTemplate(
     title="4QT IREX Log Parser",
     sidebar=sidebar,
     accent="#4099da",
+    main=[pn.panel(interactive_app)],
 )
 
 # TODO create pannels to plot each of: Motion, Temperature, Battery, Emissions.
-app = create_app(df_mdf_filtered)
-
 # TODO in sideline add summaries for: runtime, power used, power generaed, distance travelled
 
-template.main.append(indicators)
-template.main.append(app)
+# template.main.append(indicators)
 template.servable()
 
 
